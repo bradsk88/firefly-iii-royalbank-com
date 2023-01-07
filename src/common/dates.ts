a62780f4f5b9f98723e7cb92572bf57989b6ba39
Brad Johnson <bradsk88@gmail.com>
2023-01-07 15:52:29 -0600

    Adding a date util func

---
+++ src/common/dates.ts
@@ -0,0 +1,25 @@
+
+const monthIndexes: { [key: string]: number } = {
+    'Jan': 0,
+    'Feb': 1,
+    'Mar': 2,
+    'Apr': 3,
+    'May': 4,
+    'Jun': 5,
+    'Jul': 6,
+    'Aug': 7,
+    'Sep': 8,
+    'Oct': 9,
+    'Nov': 10,
+    'Dec': 11,
+}
+
+export function parseDate(dateStr: string): Date {
+    const dateParts = dateStr.split(', ');
+    const year = Number.parseInt(dateParts[1]);
+    let dayParts = dateParts[0].split(' ');
+    const monthPrf: string = dayParts[0].substring(0, 3);
+    const month = monthIndexes[monthPrf];
+    const day = Number.parseInt(dayParts[1]);
+    return new Date(year, month, day);
+}
