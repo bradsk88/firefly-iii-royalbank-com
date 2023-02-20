c36420d1427e7d0aa2bb6a8ee57d91ae5f47bb95
Brad Johnson <bradsk88@gmail.com>
2023-02-20 16:32:12 -0600

    Security: Only accept messages from the hub extension

--- src/background.ts
+++ src/background.ts
@@ -24,6 +24,7 @@ import {
     extensionSecondaryColorHex,
     hubExtensionId
 } from "./extensionid";
+import MessageSender = chrome.runtime.MessageSender;
 
 const backgroundLog = (string: string): void => {
     chrome.runtime.sendMessage({
@@ -52,7 +53,11 @@ chrome.runtime.onStartup.addListener(function () {
 setTimeout(registerSelfWithHubExtension, 1000);
 setTimeout(registerSelfWithHubExtension, 5000);
 
-chrome.runtime.onMessageExternal.addListener((msg: any, _: any, sendResponse: Function) => {
+chrome.runtime.onMessageExternal.addListener((msg: any, sender: MessageSender, sendResponse: Function) => {
+    if (sender.id != hubExtensionId) {
+        console.error("Rejecting message from unexpected extension: ", sender.id)
+        return;
+    }
     console.log('message', msg);
     if (msg.action === "login") {
         return chrome.storage.local.set({
