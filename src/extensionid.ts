0c96f17d5aa914f8d723783fcaf8436ac1fa6c58
Brad Johnson <bradsk88@gmail.com>
2023-05-17 21:44:39 -0600

    Better auto run start URL

--- src/extensionid.ts
+++ src/extensionid.ts
@@ -1,6 +1,6 @@
 export const debugAutoRun = false;
 export const bankDomain = 'royalbank.com';
-export const autoRunStartURL = 'https://royalbank.com';
+export const autoRunStartURL = 'https://secure.royalbank.com/statics/login-service-ui/index#/full/signin';
 export const extensionPrimaryColorHex = 'fedf01';
 export const extensionSecondaryColorHex = '0051a5';
 export const isSingleAccountBank = true;
